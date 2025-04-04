3162554765e0616b32bf3408687579a58fcee8ff
Murat Kirazkaya <groophylifefor@gmail.com>
2025-04-04 23:34:15 +0300

    :wrench: chore(Domain): New domain changes to website

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://yelix-docs.deno.dev/',
+  url: 'https://yelix.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
