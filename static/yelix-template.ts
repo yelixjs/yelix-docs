dd3a0a080039e4fbb60d56911b22523be4b4a691
Murat Kirazkaya <groophylifefor@gmail.com>
2025-04-05 22:09:10 +0300

    :bug: fix(yelix-template): update Deno command to include --allow-run for improved process handling

--- static/yelix-template.ts
+++ static/yelix-template.ts
@@ -3,7 +3,7 @@ import { exists } from "jsr:@std/fs/exists";
 const deno_json = `
 {
   "tasks": {
-    "dev": "deno run --watch --allow-net --allow-read --allow-env https://docs.yelix.dev/dev.ts"
+    "dev": "deno run --watch --allow-run --allow-net --allow-read --allow-env https://docs.yelix.dev/dev.ts"
   }
 }
 `.trim();
