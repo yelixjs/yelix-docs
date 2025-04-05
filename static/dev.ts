82f7cf790f4101df36f46fa6f5db230fd28262ad
Murat Kirazkaya <groophylifefor@gmail.com>
2025-04-05 21:20:06 +0300

    :robot: ci(RemoteScript): Better DX script test

---
+++ static/dev.ts
@@ -0,0 +1,39 @@
+// dev.ts
+
+const resolveProcess = new Deno.Command("deno", {
+  args: [
+    "run",
+    "--allow-read",
+    "--allow-write",
+    "https://docs.yelix.dev/resolveEndpoints.ts",
+    "--folder",
+    "api",
+  ],
+  stdin: "null",
+  stdout: "piped",
+  stderr: "piped",
+}).spawn();
+
+console.log("[dev] Started resolveEndpoints.ts");
+
+const startProcess = new Deno.Command("deno", {
+  args: [
+    "run",
+    "--watch=*",
+    "--allow-net",
+    "--allow-read",
+    "--allow-env",
+    "main.ts",
+  ],
+  stdin: "inherit",
+  stdout: "inherit",
+  stderr: "inherit",
+});
+
+const { code } = await startProcess.output();
+
+console.log(`[dev] main.ts exited with code ${code}, killing resolveEndpoints...`);
+
+resolveProcess.kill("SIGTERM");
+
+Deno.exit(code);
---
+++ static/resolveEndpoints.ts
@@ -0,0 +1,74 @@
+import { parseArgs } from "jsr:@std/cli/parse-args";
+import * as path from "jsr:@std/path@1.0.8";
+
+console.log("[resolveEndpoints] running at", new Date().toISOString());
+
+async function walkDirectory(
+  directoryPath: string,
+  fileHandler: (args: { path: string; fileInfo: Deno.FileInfo }) => void,
+) {
+  const fileInfo = await Deno.lstat(directoryPath);
+  fileHandler({ fileInfo, path: directoryPath });
+
+  if (fileInfo.isDirectory) {
+    const entries = await Deno.readDir(directoryPath);
+    for await (const entry of entries) {
+      await walkDirectory(`${directoryPath}\\${entry.name}`, fileHandler);
+    }
+  }
+}
+
+function getFolderFromArgs(): string {
+  const args = parseArgs(Deno.args);
+  const targetFolder = args.folder || args.f;
+
+  if (!targetFolder) {
+    console.error("Please provide a folder name using --folder or -f.");
+    Deno.exit(1);
+  }
+
+  return targetFolder;
+}
+
+function generateImportStatement(filePath: string, basePath: string): string {
+  const relativePath = "." + filePath
+    .replace(basePath, "")
+    .replaceAll("\\", "/")
+    .replaceAll("//", "/");
+  return `  await import("${relativePath}"),\n`;
+}
+
+async function generateEndpointsFile() {
+  const currentDir = Deno.cwd();
+  const targetFolder = getFolderFromArgs();
+  const rootPath = path.join(currentDir, targetFolder);
+
+  let endpointsContent = "const endpoints = [\n";
+
+  await walkDirectory(rootPath, (file) => {
+    if (file.fileInfo.isFile) {
+      endpointsContent += generateImportStatement(file.path, currentDir);
+    }
+  });
+
+  endpointsContent += "\n];\nexport default endpoints;\n";
+  await Deno.writeTextFile(
+    path.join(currentDir, "endpoints.ts"),
+    endpointsContent,
+  );
+}
+
+async function watch() {
+  const watcher = Deno.watchFs(Deno.cwd());
+  for await (const event of watcher) {
+    const filePath = event.paths[0];
+    const fileName = path.basename(filePath);
+    if (event.kind === "modify" && fileName !== "endpoints.ts") {
+      console.log("[resolveEndpoints] file modified" ,fileName);
+      generateEndpointsFile();
+    }
+  }
+}
+
+generateEndpointsFile();
+watch();
