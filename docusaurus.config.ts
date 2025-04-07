2395014e9d3cdff2e87e837dec36c5c5fb790841
Murat Kirazkaya <groophylifefor@gmail.com>
2025-04-07 18:38:21 +0300

    :art: fix(config): update social card image format from JPG to PNG

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/yelix-social-card.jpg',
+    image: 'img/yelix-social-card.png',
     navbar: {
       title: 'Yelix',
       logo: {
