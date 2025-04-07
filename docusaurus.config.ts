74562b7d2bbab658e1ef07578e130fdbc23b079d
Murat Kirazkaya <groophylifefor@gmail.com>
2025-04-07 18:35:29 +0300

    Refactor code structure for improved readability and maintainability

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/yelix-social-card.jpg',
     navbar: {
       title: 'Yelix',
       logo: {
