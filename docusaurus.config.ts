7be0a90cfffde9c0f63e1c1e53f0c607ad0d02b7
Murat Kirazkaya <groophylifefor@gmail.com>
2025-04-11 15:19:04 +0300

    :art: feat(analytics): add Google Analytics tracking configuration and plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -60,6 +60,10 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-T3T2K4LTPC',
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
